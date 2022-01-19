ed9fda38627cbae1f6c67b6b62b2e3b2de4d63b5
lotteph <lotte.philippus@gmail.com>
2022-01-19 16:23:46 +0100

    uploaded latest version of front-end

--- map.py
+++ map.py
@@ -5,16 +5,49 @@ import json
 import urllib.parse
 import matplotlib.pyplot as plt
 import seaborn as sns
-import predict as h
+import map_helper as h
 import datetime as dt
+from datetime import timedelta
 import xgboost as xgb
 import os
+import warnings
 import glob
+import plotly
+import plotly.tools as tls
+
+warnings.filterwarnings('ignore')
+
+begin_time = dt.datetime.now()
 
 IMG_RATIO = 1280/380
 
 IMG_HEIGHT = int(880 / IMG_RATIO)
 IMG_WIDTH = int(1280 / IMG_RATIO)
+
+park_locations = {
+'Vondelpark West' : [{'lat': 52.356496, 'lng': 4.861447}],
+'Vondelpark Oost 3' : [{'lng': 4.869217, 'lat': 52.358252}],
+'Vondelpark Oost 2' : [{'lng': 4.874692, 'lat': 52.359798}],
+'Vondelpark Oost 1' : [{'lng': 4.879652, 'lat': 52.360991}],
+'Oosterpark' : [{'lng': 4.920558, 'lat': 52.360098}],
+'Sarphatipark' : [{'lng': 4.896375, 'lat': 52.354364}],
+'Westerpark West' : [{'lng': 4.867128, 'lat': 52.387099}],
+'Westerpark Centrum' : [{'lng': 4.873268, 'lat': 52.387374}],
+'Westerpark Oost' : [{'lng': 4.878379, 'lat': 52.386379}],
+'Westergasfabriek' : [{'lng': 4.869769, 'lat': 52.385920}],
+'Rembrandtpark Noord' : [{'lng': 4.846573, 'lat': 52.366664}],
+'Rembrandtpark Zuid' : [{'lng': 4.846932, 'lat': 52.361161}],
+'Erasmuspark' : [{'lng': 4.851909, 'lat': 52.374808}],
+'Amstelpark' : [{'lng': 4.894404, 'lat': 52.330409}],
+'Park Frankendael' : [{'lng': 4.929839, 'lat': 52.350703}],
+'Beatrixpark' : [{'lng': 4.881352, 'lat': 52.342471}],
+'Flevopark' : [{'lng': 4.947881, 'lat': 52.360087}],
+'Gaasperpark' : [{'lng': 4.992192, 'lat': 52.310420}],
+'Nelson Mandelapark' : [{'lng': 4.963691, 'lat': 52.312204}],
+'Noorderpark' : [{'lng': 4.919606, 'lat': 52.392651}],
+'Sloterpark' : [{'lng': 4.811894, 'lat': 52.366219}],
+'WH Vliegenbos' : [{'lng': 4.931495, 'lat': 52.388802}]
+}
 
 if not os.path.exists("images"):
     os.mkdir("images")
@@ -23,7 +56,7 @@ def get_location_url(location):
     query = location + ', Amsterdam'
     return urllib.parse.quote(query)
 
-def get_html(data_now, data_next):
+def get_html(data_now, data_next, park_suggs):
     html = f'''
         <html>
             <head>
@@ -36,24 +69,30 @@ def get_html(data_now, data_next):
                 </div>
 
                 <div class='park_predictions'>
-                    <div class='people'>
-                        {int(data_now.Visits)} people
+                    <div class='data'>
+                        <div class='people'>
+                            {int(data_now.Visits)} people
+                        </div>
+
+                        <div class='text_pred'>
+                            Measured at {data_now.Time[:5]}
+                        </div>
+
+                        <div class='people'>
+                            {int(data_next.loc['Predicted visitors'])} people
+                        </div>
+
+                        <div class ='text_pred'>
+                            Expected in the next hour
+                        </div>
                     </div>
 
-                    <div class='text_pred'>
-                        Measured at {data_now.Time[:5]}
-                    </div>
-
-                    <div class='people'>
-                        {int(data_next.loc['Predicted visitors'])} people
-                    </div>
-
-                    <div class ='text_pred'>
-                        Expected in the next hour
+                    <div class='park_business'>
+                        {baseline[baseline['Location'] == data_now.Location]['Business'].values[0]}
                     </div>
 
                     <div class='image'>
-                        <img id='predictions' src="images/{data_now.Location}.png" alt="{data_now.Location}", height={IMG_HEIGHT} width={IMG_WIDTH}><br>
+                        <img id='predictions' src="images/{data_now.Location}.png" alt="{data_now.Location}" height={IMG_HEIGHT} width={IMG_WIDTH}><br>
                     </div>
 
                     <div class='mapslink'>
@@ -66,9 +105,19 @@ def get_html(data_now, data_next):
                         Top 3 park suggestions:
                     </div>
                     <div class='text_sugg'>
-                        -Park 1<br>
-                        -Park 2<br>
-                        -Park 3<br>
+                        -<a id='mapslink2' href=https://www.google.com/maps/dir/?api=1&origin={get_location_url(data_now.Location)}&destination={get_location_url(park_suggs.iloc[0, 0])}, target="_blank">{park_suggs.iloc[0, 0]}<br>
+                        -<a id='mapslink2' href=https://www.google.com/maps/dir/?api=1&origin={get_location_url(data_now.Location)}&destination={get_location_url(park_suggs.iloc[1, 0])}, target="_blank">{park_suggs.iloc[1, 0]}<br>
+                        -<a id='mapslink2' href=https://www.google.com/maps/dir/?api=1&origin={get_location_url(data_now.Location)}&destination={get_location_url(park_suggs.iloc[2, 0])}, target="_blank">{park_suggs.iloc[2, 0]}<br>
+                    </div>
+                    <div class='business_sugg'>
+                        {baseline[baseline['Location'] == park_suggs.iloc[0, 0]]['Business'].values[0]}<br>
+                        {baseline[baseline['Location'] == park_suggs.iloc[1, 0]]['Business'].values[0]}<br>
+                        {baseline[baseline['Location'] == park_suggs.iloc[2, 0]]['Business'].values[0]}<br>
+                    </div>
+                    <div class='distance'>
+                        {park_suggs.iloc[0, 1]} km<br>
+                        {park_suggs.iloc[1, 1]} km<br>
+                        {park_suggs.iloc[2, 1]} km<br>
                     </div>
                 </div>
                 </div>
@@ -88,9 +137,11 @@ def create_plot(prev, data, file_name):
     f, axes = plt.subplots()
     plt.plot(prev['Visits'], 'o-', linewidth=8, color='#b52222', markersize=15, markerfacecolor='#661111')
     plt.plot(data['Predicted visitors'], 'o:', color='#b52222', linewidth=8, markersize=15, markerfacecolor='#661111')
+    plt.axhline(y=baseline[baseline['Location'] == file_name]['Visits'].values[0], color='#b04343', linestyle='-', linewidth=4)
     # plt.plot(data['Actual visitors'], label='Actual visitors')
+
     plt.setp(axes, xticks=np.linspace(0,12,4),
-         xticklabels=[prev['Time'][0], prev['Time'][4], prev['Time'][-1], next_dt.strftime('%H:%M:%S')])
+         xticklabels=[prev['Time'][0], prev['Time'][int(len(prev) / 2)], prev['Time'][-1], next_dt.strftime('%H:%M:%S')])
 
     # plt.xlabel('Time')
     # plt.ylabel('Visitors')
@@ -99,6 +150,8 @@ def create_plot(prev, data, file_name):
     # plt.legend(loc='upper left', prop={'size': 25})
 
     plt.savefig('images/'+file_name, transparent=True)
+    plotly_fig = tls.mpl_to_plotly(f)
+    plotly.offline.plot(plotly_fig, filename='test.html', auto_open=False, config={'displayModeBar': False})
 
     plt.cla()
     plt.clf()
@@ -130,30 +183,91 @@ def get_data(data):
     idx_prev = dt.datetime(2022, 1, 5, time.hour - 2, time.minute, 0)
     idx_next = dt.datetime(2022, 1, 5, time.hour + 1, time.minute, 0)
 
-    prev = data.loc[str(idx_prev):str(idx)]
-    next = data.loc[str(idx):str(idx_next)]
+    if time.hour == 23:
+        prev = data.loc[str(idx_prev):str(idx)]
+        next = data.loc[str(idx):]
+    elif time.hour < 2:
+        prev = data.loc[:str(idx)]
+        next = data.loc[str(idx):str(idx_next)]
+    else:
+        prev = data.loc[str(idx_prev):str(idx)]
+        next = data.loc[str(idx):str(idx_next)]
 
     return prev, next
+
+def create_park_suggestions(data, model, current_location):
+    prediction_date = dt.datetime(2022, 1, 5)
+    time = dt.datetime.now()
+    prediction_time = dt.datetime(2022, 1, 5, time.hour, time.minute, 0)
+    time = h.ceil_dt(prediction_time + dt.timedelta(minutes=15), dt.timedelta(minutes=15))
+
+    current_lat = park_locations[current_location][0]['lat']
+    current_lng = park_locations[current_location][0]['lng']
+
+    preds = {}
+    for loc in locations:
+        pred_data = data.loc[str(time)]
+        pred = get_predictions(model, pred_data, loc)
+        preds[loc] = pred['Predicted visitors'].values[0]
+
+    values = list(preds.values())
+    park_suggestion = h.getParkSuggestion(resono, prediction_date, current_lat, current_lng, values, time)
+
+    return park_suggestion
+
+def get_baseline(data, resono):
+    prediction_date = dt.datetime(2022, 1, 5)
+    time = dt.datetime.now()
+    time = time - dt.timedelta(minutes=time.minute % 15, seconds=time.second)
+    idx = dt.datetime(2022, 1, 5, time.hour, time.minute, 0)
+    current_data = data.loc[str(idx)][['Location', 'Visits']]
+
+    baseline = h.get_baseline(resono, prediction_date, time)
+    business = []
+
+    for loc in locations:
+        if current_data[current_data['Location'] == loc]['Visits'].values[0] < 0.9 * baseline[baseline['Location'] == loc]['Visits'].values[0]:
+            business.append('Not busy')
+        elif current_data[current_data['Location'] == loc]['Visits'].values[0] > 1.1 * baseline[baseline['Location'] == loc]['Visits'].values[0]:
+            business.append('Busier than usual')
+        else:
+            business.append('A little busy')
+
+    baseline['Business'] = business
+    return baseline
+
+data = pd.read_csv('CSVs/Resono_jan5.csv', index_col=0)
+locations = data.Location.unique()
+
+resono = pd.read_csv('CSVs/resono_2020_2022.csv', index_col=0)
 
 d = get_geodict('GeoJSON files/*.geojson')
 geos = list(d.keys())
 
-data = pd.read_csv('CSVs/Resono_test.csv', index_col=0)
-
 print('Loading model...')
 model = xgb.Booster()
-model.load_model("model.json")
+model.load_model("shittymodel.json")
 print('Done!')
 
+baseline = get_baseline(data, resono)
 
-locations = data.Location.unique()
-chordata = data[data['Time'] == get_time().strftime('%H:%M:%S')][['Location', 'Visits']]
-chordata.set_index('Location')
+# ~~~~~  MAP IMPLEMENTATION  ~~~~~
 
-style_function = lambda x: {'fillColor': '#ffffff',
+style_function_gr = lambda x: {'fillColor': '#00ff00',
                             'color':'#000000',
-                            'fillOpacity': 0.0,
+                            'fillOpacity': 0.4,
                             'weight': 0.5}
+
+style_function_yl = lambda x: {'fillColor': '#ffff00',
+                            'color':'#000000',
+                            'fillOpacity': 0.4,
+                            'weight': 0.5}
+
+style_function_rd = lambda x: {'fillColor': '#ff0000',
+                            'color':'#000000',
+                            'fillOpacity': 0.4,
+                            'weight': 0.5}
+
 highlight_function = lambda x: {'fillColor': '#000000',
                                 'color':'#000000',
                                 'fillOpacity': 0.5,
@@ -165,22 +279,6 @@ m = folium.Map(
     zoom_start=13,
 )
 
-chor = folium.Choropleth(
-    geo_data=d['all_parks'],
-    name="choropleth",
-    data=chordata,
-    columns=["Location", "Visits"],
-    key_on="properties.Naam",
-    bins=6,
-    fill_color="YlOrRd",
-    fill_opacity=0.8,
-    line_opacity=0.7,
-    legend_name="Visitors",
-    highlight=True,
-)
-
-chor.add_to(m)
-
 fgp = folium.FeatureGroup(name="Parks")
 
 print('Making predictions for the parks...')
@@ -189,18 +287,28 @@ for park in geos:
     g = d[park]
     name = g['features'][0]['properties']['Naam']
     if name in locations:
+        data_loc = data[data['Location'] == name]
+        prev, next = get_data(data_loc)
+        preds = get_predictions(model, next, name)
+        park_suggs = create_park_suggestions(data, model, name)
+        create_plot(prev, preds, name)
+
+        if baseline[baseline['Location'] == name][['Business']].values[0] == 'Not busy':
+            style_function = style_function_gr
+        elif baseline[baseline['Location'] == name][['Business']].values[0] == 'Busier than usual':
+            style_function = style_function_rd
+        else:
+            style_function = style_function_yl
+
+        html = get_html(prev.iloc[-1], preds.iloc[-1], park_suggs)
+        test = folium.Html(html, script=True)
+        popup = folium.Popup(test, max_width=400)
+
         gjson = folium.GeoJson(g,
                                name=name,
                                style_function=style_function,
                                highlight_function=highlight_function,
                               )
-        data_loc = data[data['Location'] == name]
-        prev, next = get_data(data_loc)
-        preds = get_predictions(model, next, name)
-        create_plot(prev, preds, name)
-        html = get_html(prev.iloc[-1], preds.iloc[-1])
-        test = folium.Html(html, script=True)
-        popup = folium.Popup(test, max_width=400)
         gjson.add_child(popup)
 
         fgp.add_child(gjson)
@@ -208,3 +316,5 @@ for park in geos:
 fgp.add_to(m)
 print('Done!')
 m.save('index.html')
+
+print(dt.datetime.now() - begin_time)
--- map_helper.py
+++ map_helper.py
@@ -6,11 +6,38 @@ import seaborn as sns
 import os
 import datetime as dt
 import xgboost as xgb
+# import category_encoders as ce
 from sklearn.svm import OneClassSVM
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
+from math import radians, cos, sin, asin, sqrt
+from dateutil.relativedelta import relativedelta
+from datetime import timedelta
+
+vondelpark_west = [{'lat': 52.356496, 'lng': 4.861447}]
+vondelpark_oost_3 = [{'lng': 4.869217, 'lat': 52.358252}]
+vondelpark_oost_2 = [{'lng': 4.874692, 'lat': 52.359798}]
+vondelpark_oost_1 = [{'lng': 4.879652, 'lat': 52.360991}]
+oosterpark = [{'lng': 4.920558, 'lat': 52.360098}]
+sarphatipark = [{'lng': 4.896375, 'lat': 52.354364}]
+westerpark_west = [{'lng': 4.867128, 'lat': 52.387099}]
+westerpark_centrum = [{'lng': 4.873268, 'lat': 52.387374}]
+westerpark_oost = [{'lng': 4.878379, 'lat': 52.386379}]
+westergasfabriek = [{'lng': 4.869769, 'lat': 52.385920}]
+rembrandtpark_noord = [{'lng': 4.846573, 'lat': 52.366664}]
+rembrandtpark_zuid = [{'lng': 4.846932, 'lat': 52.361161}]
+erasmuspark = [{'lng': 4.851909, 'lat': 52.374808}]
+amstelpark = [{'lng': 4.894404, 'lat': 52.330409}]
+park_frankendael = [{'lng': 4.929839, 'lat': 52.350703}]
+beatrixpark = [{'lng': 4.881352, 'lat': 52.342471}]
+flevopark = [{'lng': 4.947881, 'lat': 52.360087}]
+gaasperpark = [{'lng': 4.992192, 'lat': 52.310420}]
+nelson_mandelapark = [{'lng': 4.963691, 'lat': 52.312204}]
+noorderpark = [{'lng': 4.919606, 'lat': 52.392651}]
+sloterpark = [{'lng': 4.811894, 'lat': 52.366219}]
+wh_vliegenbos = [{'lng': 4.931495, 'lat': 52.388802}]
 
 def clean_resono(df, merge=True):
     '''
@@ -215,3 +242,78 @@ def predict(model, data, location, pred_params):
     # print("RMSE : % f" %(rmse))
     # print("MAE : % f" %(mae))
     return predictions.sort_index()
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+def ceil_dt(dt, delta):
+    return dt + (dt.min - dt) % delta
+
+def getParkSuggestion(df, date, lat, lng, pred, time):
+    df = clean_resono(df)
+    resono = df.reset_index()
+    resono['Location'] = resono['Location'].str.replace('W.H. Vliegenbos', 'WH Vliegenbos')
+    # print(resono)
+
+    prediction_date_4_months = date + relativedelta(months=-4, days=-1)
+    df_4months = resono[(resono['Datetime'] >= prediction_date_4_months) & (resono['Datetime'] < date)]
+
+    df_4months['Weekday'] = df_4months['Datetime'].apply(lambda x: x.weekday())
+    df_4months_groupby = df_4months.groupby(['Time', 'Weekday', 'Location']).mean()
+
+    df_4months_groupby = df_4months_groupby.reset_index()
+    df_baseline = df_4months_groupby[(df_4months_groupby['Weekday'] == date.weekday()) &
+                                  (df_4months_groupby['Time'] == time.strftime("%H:%M:%S"))]
+
+    df_baseline['Longitude'] = df_baseline['Location'].apply(lambda x: globals()["_".join(f"{x.lower()}".split())][0]['lng'])
+    df_baseline['Latitude'] = df_baseline['Location'].apply(lambda x: globals()["_".join(f"{x.lower()}".split())][0]['lat'])
+
+    df_baseline['Distance'] = [haversine(lat, lng, df_baseline.iloc[x]['Latitude'], df_baseline.iloc[x]['Longitude'])
+                         for x in range(df_baseline.shape[0])]
+
+    df_baseline['Predictions'] = pred
+    df_baseline['Crowdedness factor'] = (df_baseline['Predictions'] - df_baseline['Visits']) / df_baseline['Visits'] #(baseline - values) / values
+    df_baseline['Park suggestion'] = df_baseline['Distance'] + (df_baseline['Crowdedness factor']*5)
+    df_baseline['Distance'] = round(df_baseline['Distance'],2)
+
+    df_baseline = df_baseline.reset_index(drop=True)
+    df_baseline.index += 1
+    df_baseline = df_baseline[df_baseline['Distance'] > 0]
+
+    if(df_baseline['Distance'] <= 3).sum() >= 3:
+        df_baseline = df_baseline[df_baseline['Distance'] <= 3]
+        return df_baseline.sort_values(by='Park suggestion')[['Location', 'Distance', 'Park suggestion']].iloc[:3]
+    else:
+        df_park_suggestion = df_baseline[df_baseline['Distance'] <= 3].sort_values(by='Park suggestion')
+        return pd.concat([df_park_suggestion, df_baseline.sort_values(by='Distance')[df_park_suggestion.shape[0]:]], axis=0)[['Location', 'Distance', 'Park suggestion']].iloc[:3]
+
+def get_baseline(df, date, time):
+    df = clean_resono(df)
+    resono = df.reset_index()
+    resono['Location'] = resono['Location'].str.replace('W.H. Vliegenbos', 'WH Vliegenbos')
+    # print(resono)
+
+    prediction_date_4_months = date + relativedelta(months=-4, days=-1)
+    df_4months = resono[(resono['Datetime'] >= prediction_date_4_months) & (resono['Datetime'] < date)]
+
+    df_4months['Weekday'] = df_4months['Datetime'].apply(lambda x: x.weekday())
+    df_4months_groupby = df_4months.groupby(['Time', 'Weekday', 'Location']).mean()
+
+    df_4months_groupby = df_4months_groupby.reset_index()
+    df_baseline = df_4months_groupby[(df_4months_groupby['Weekday'] == date.weekday()) &
+                                  (df_4months_groupby['Time'] == time.strftime("%H:%M:%S"))]
+
+    return df_baseline
